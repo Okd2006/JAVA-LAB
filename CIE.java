1a7bac3b18ded97c383e5272b7e1a70eb1a0fe9f
Okd2006 <omkrrishbgs@gmail.com>
2025-12-17 20:44:26 +0530

    Update CIE.java

--- CIE.java
+++ CIE.java
@@ -1,1 +1,11 @@
-.
+package Program6_Package.CIE;
+public class Student {
+    public String usn, name;
+    public int sem;
+
+    public Student(String usn, String name, int sem){
+        this.usn = usn;
+        this.name = name;
+        this.sem = sem;
+    }
+}
